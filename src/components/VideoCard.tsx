1e9caac846ff652dc5f399964302ba734a2a4a97
akashrajakku <akashrajakku263@gmail.com>
2024-08-17 22:35:31 +0530

    initial setup

---
+++ src/components/VideoCard.tsx
@@ -0,0 +1,11 @@
+export function VideoCard(){
+    return(
+        <div>
+            <img src="/photo.jpg"></img>
+            <div className="grid grid-cols-12">
+                <div className="col-span-2"><img src="/photo.jpg"></img></div>
+                <div className="col-span-10">Premanand Ji Maharaj on kaam, krodha, Lobha</div>
+            </div>
+        </div>
+    )
+}
